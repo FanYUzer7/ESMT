364ff79bea1faaccd41e37e43d9e7c7be2df8831
fyz <fyz@thinkpad.com>
2022-11-23 16:31:02 +0800

    修改了insert时mbr和hash的更新方式，（大概）减少了mbr和hash的计算次数；抽象了insert方法，使得能够插入ESMTEntry；实现了delete的所有细节；TODO: 测试delete方法的正确性

--- MerkleRTree/src/node.rs
+++ MerkleRTree/src/node.rs
@@ -3,7 +3,7 @@ use std::fmt::Debug;
 use types::hash_value::{ESMTHasher, HashValue};
 use crate::shape::Rect;
 
-pub type ValueSpace = usize;
+pub type ValueSpace = f32;
 /// `ObjectEntry`表示`ESMT`中的一个空间对象，只存在于叶子节点中。
 pub struct ObjectEntry<V, const D: usize>
     where
@@ -266,30 +266,31 @@ impl<const D: usize, const C: usize> Node<ValueSpace, D, C> {
         subtree_idx
     }
 
-    pub fn insert(&mut self, obj: ObjectEntry<ValueSpace, D>, height: u32) {
+    /// 插入，重新计算当前层的mbr以及下一层的hash
+    pub fn insert(&mut self, obj: ESMTEntry<ValueSpace, D, C>, loc: &Rect<ValueSpace, D>, height: u32) {
         if height == 0 {
             if self.entry.is_empty() {
-                self.entry.push(ESMTEntry::Object(obj));
+                self.entry.push(obj);
                 self.recalculate_mbr();
             } else {
-                self.mbr.expand(obj.loc());
-                self.entry.push(ESMTEntry::Object(obj));
+                self.mbr.expand(&loc);
+                self.entry.push(obj);
             }
         } else {
-            let subtree_idx = self.choose_subtree(obj.loc());
+            let subtree_idx = self.choose_subtree(&loc);
             let node_mut = self.entry[subtree_idx].get_node_mut();
-            node_mut.insert(obj, height - 1);
+            node_mut.insert(obj, loc, height - 1);
             // need to split
             if node_mut.entry.len() > Self::CAPACITY {
                 // 分裂并重新计算mbr
                 let new_node = node_mut.split_by_hilbert_sort();
+                self.mbr.expand(new_node.mbr());
                 self.entry.push(ESMTEntry::ENode(new_node));
-                self.recalculate_mbr();
             } else {
-                self.mbr.expand(node_mut.mbr());
+                node_mut.rehash();
             }
+            self.mbr.expand(&loc);
         }
-        self.rehash();
     }
 
     fn split_by_hilbert_sort(&mut self) -> Node<ValueSpace, D, C> {
@@ -503,9 +504,14 @@ impl<const D: usize, const C: usize> MerkleRTree<ValueSpace, D, C> {
         if self.root.is_none() {
             self.root = Some(Node::new_with_height(0));
         }
-        let obj = ObjectEntry::new(key, loc, hash);
+        let obj = ESMTEntry::Object(ObjectEntry::new(key, loc, hash));
+        let obj_loc = obj.mbr().clone();
+        self.insert_impl(obj, &obj_loc, self.height);
+    }
+
+    fn insert_impl(&mut self, entry: ESMTEntry<ValueSpace, D, C>, loc: &Rect<ValueSpace, D>, height: u32) {
         let root = self.root.as_mut().unwrap();
-        root.insert(obj, self.height);
+        root.insert(entry, loc, self.height);
         if root.is_overflow() {
             self.height += 1;
             let mut new_root = Node::new_with_height(self.height);
@@ -515,6 +521,8 @@ impl<const D: usize, const C: usize> MerkleRTree<ValueSpace, D, C> {
             new_root.entry.push(ESMTEntry::ENode(another));
             new_root.recalculate_state_after_sort();
             self.root = Some(new_root);
+        } else {
+            root.rehash();
         }
         self.len += 1;
     }
@@ -549,7 +557,15 @@ impl<const D: usize, const C: usize> MerkleRTree<ValueSpace, D, C> {
     }
 
     fn reinsert(&mut self, reinsert_list: Vec<ESMTEntry<ValueSpace, D, C>>) {
-
+        for entry in reinsert_list.into_iter().rev() {
+            let entry_loc = entry.mbr().clone();
+            let expected_height_to_insert = if entry.is_node() {
+                self.height - entry.get_node().height - 1
+            } else {
+                self.height
+            };
+            self.insert_impl(entry, &entry_loc, expected_height_to_insert);
+        }
     }
 
     pub fn display(&self) -> (Vec<(u32, Rect<ValueSpace, D>)>, Vec<Rect<ValueSpace, D>>) {
@@ -584,13 +600,14 @@ mod test {
     use rand::{thread_rng, Rng};
     use types::hash_value::{ESMTHasher, HashValue};
     use crate::node::{ESMTEntry, HilbertSorter, ObjectEntry, ValueSpace};
+    use crate::node::MerkleRTree as Tree;
 
     #[test]
     fn test_efficient() {
         let mut time = 0;
         for _ in 0..100 {
             let mut v = generate_random_rect();
-            let sorter = HilbertSorter::<2, 40>::new(&Rect::new([0f32, 0f32], [100f32, 100f32]));
+            let sorter = HilbertSorter::<2, 40>::new(&Rect::new([0, 0], [100, 100]));
             let start = Instant::now();
             let _sorted_v = {
                 v.sort_by(|a, b| {
@@ -608,7 +625,7 @@ mod test {
         let mut time_p = 0;
         for _ in 0..100 {
             let v = generate_random_rect();
-            let sorter = HilbertSorter::<2, 40>::new(&Rect::new([0f32, 0f32], [100f32, 100f32]));
+            let sorter = HilbertSorter::<2, 40>::new(&Rect::new([0, 0], [100, 100]));
             let start = Instant::now();
             let mut sorted_v = {
                 let mut m = v.into_iter().map(|e| (sorter.hilbert_idx(e.mbr()), e)).collect::<Vec<_>>();
@@ -627,12 +644,13 @@ mod test {
         let mut rng = thread_rng();
         let mut v = vec![];
         for _ in 0..1000 {
-            let p = rng.gen_range(0f32..100_f32);
+            let p = rng.gen_range(0..100);
             v.push(ESMTEntry::Object(ObjectEntry::new("key".to_string(), [p, p], HashValue::zero())));
         }
         v
     }
 
+    #[test]
     fn test_root_hash() {
         // let mut rng = thread_rng();
         let points = vec![
